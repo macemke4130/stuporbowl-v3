9c6dc4e3433f019689113774aeab9c4ba87f6f90
macemke4130 <lucasmace4130@gmail.com>
2026-09-13 21:23:44 -0500

    Update rss title

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get("/rss.xml", async (req, res) => {
 
     // 4. Build RSS XML
     const feed = new RSS({
-      title: "Your Site RSS Feed",
+      title: "Stupor Bowl Dot Org site updates.",
       description: "Combined updates from database and static Astro pages",
       feed_url: "https://www.stuporbowl.org/rss.xml",
       site_url: "https://www.stuporbowl.org",
